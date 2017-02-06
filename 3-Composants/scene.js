11c140a30bc164cfb8705c1c859a29d7f2dce7a6
Matthieu CROUZET <Matthieu CROUZET>
2017-02-05 22:33:30 -0500

    debug

--- 3-Composants/scene.js
+++ 3-Composants/scene.js
@@ -17,52 +17,64 @@ define([
     // retourne une promesse résolue lorsque l'ensemble de la
     // hiérarchie est configurée correctement.
     static create(description) {
-		var promise = new Promise(
-			function(resolve, reject) {  				
-			  const scene = new Scene();
-			  if(Object.keys(description).length > 0){
-				scene.setup(description);
-			  }
-			  resolve(scene);
-			});
-		  return promise;
-    }
+		var promises = [];
+		const scene = new Scene();
+		var keys = Object.keys(description);
+		for(var i=0; i < keys.length; i++){
+			promises.push(
+				SceneObject.create(	keys[i], 
+									description[keys[i]].components, 
+									description[keys[i]].children, 
+									scene));
+		};
+		return Promise.all(promises).then((sceneObjects) => {
+			for(var obj in sceneObjects){
+				scene.children[sceneObjects[obj].name] = sceneObjects[obj];
+			}
+		}).then(function(){
+			var promises = [];
+			for(var i in scene.children){
+				scene.children[i].setup(description[scene.children[i].name], promises);
+			};
+			return Promise.all(promises);
+		}).then(() => {
+			return scene;
+		});
+	}
 
     // ## Méthode *display*
     // Cette méthode appelle les méthodes *display* de tous les
     // objets de la scène.
     display(dT) {
-		this.children.forEach(function(key){
-			if(key instanceof SceneObject)
-				key.display(dT);
-		});
+	  for (var keys in this.children){
+		this.children[keys].display(dT);
+      }
     }
 
     // ## Méthode *update*
     // Cette méthode appelle les méthodes *update* de tous les
     // objets de la scène.
     update(dT) {
-		this.children.forEach(function(key){
-			if(key instanceof SceneObject)
-				key.update(dT);
-		});
+	  for (var keys in this.children){
+		this.children[keys].update(dT);
+      }
     }
 
     // ## Fonction *findObject*
     // La fonction *findObject* retourne l'objet de la scène
     // portant le nom spécifié.
-    findObject(objectName) {
-		var values = [];
-		this.children.forEach(function(key){
-			if(key instanceof SceneObject)
-				values.push(key.findObjectInObject(objectName));
-		});
-		for(var i = 0; i < values.length; i++){
-			if(values[i] != null){
-				return values[i];
-			}
-		}
-		return null;
+    findObject(objectName, where = this.children) {
+      if(where[objectName]){
+        return where[objectName];
+      } else {
+        for (var i in where){
+          var children = where[i].children;
+          var objectFound = this.findObject(objectName,children);
+          if(objectFound){
+            return objectFound;
+          }
+        }
+      }
     }
 	
 	// ### Constructeur de la classe *Scene*
@@ -70,22 +82,31 @@ define([
 	constructor(){	
 		this.name = "scene";
 		this.children = [];
+		/*
 		var background = SceneObject.create("Background", "background", this);
 		var player1 = SceneObject.create("Player", "player1", this);
 		var player2 = SceneObject.create("Player", "player2", this);		
 		var ball = SceneObject.create("Ball", "ball",this);
 		var referee = SceneObject.create("Referee", "referee", this);
 		this.children.push(background,player1, player2, ball, referee);
+		*/
 	}
 	
 	// ### Méthode *setup*
     // La méthode *setup** met en place la scene
-	setup(descr){
-		this.children[0].setup(descr["background"]);
-		this.children[1].setup(descr["player1"]);
-		this.children[2].setup(descr["player2"]);
-		this.children[3].setup(descr["ball"]);
-		this.children[4].setup(descr["referee"]);
+	setup(descr, promises){
+		var children = this.children;
+		Object.keys(descr).forEach((key) => {
+			children.push(new SceneObject(key,this));
+		});
+		this.children = children;
+		var promises = [];
+		this.children.forEach(function(key, value){
+			if(key instanceof SceneObject){
+				key.setup(descr[key.name], promises);
+			}
+		});
+		return Promise.all(promises);
 	}
 	
   }
--- 3-Composants/sceneObject.js
+++ 3-Composants/sceneObject.js
@@ -10,29 +10,30 @@ define([
   // peut contenir des enfants et des composants.
   class SceneObject {
 	  
-	static create(type, name, descr, owner) {
-		let sceneObjectCreators = {
-			Background: BackgroundObject,
-			Ball: BallObject,
-			Player: PlayerObject,
-			Referee: RefereeObject,
-			Score: ScoreObject
-		};  
-      const obj = new sceneObjectCreators[type](name, descr,owner);
-      obj.type = type;
-      return obj;
-    }
-	  
-
-	  
+	
+	static create(name, components, children, owner){
+      const sceneObject = new SceneObject(name,owner);
+        if(components){
+		  var keys = Object.keys(components);
+          for(var i=0; i < keys.length; i++){
+            sceneObject.addComponent(keys[i]);
+          };
+        }
+        if (children){
+		  var keys = Object.keys(children);
+		  for(var i=0; i < keys.length; i++){
+            const object = SceneObject.create(keys[i], children[keys[i]].components, children[keys[i]].children, this);
+            sceneObject.addChild(keys[i], object);
+          };
+        }
+        return sceneObject;
+  }  
 	  
     // ## Méthode *addComponent*
     // Cette méthode prend en paramètre le type d'un composant et
     // instancie un nouveau composant.
     addComponent(type) {
-      const newComponent = ComponentFactory.create(type, this);
-	  this.components.push(newComponent);
-	  return newComponent;
+	  this.components[type] =  ComponentFactory.create(type, this);
     }
 	
 	// ### Constructeur de la classe *SceneObject*
@@ -49,40 +50,21 @@ define([
     // Cette fonction retourne un composant existant du type spécifié
     // associé à l'objet.
     getComponent(type) {
-	  var comp = null;
-	  this.components.forEach((key,value) => {
-		  if(comp){
-			  return;
-		  }
-		  if(key.type == type){
-			  comp = key;
-		  }
-	  });
-	  return comp;
+	  return this.components[type];
     }
 
     // ## Méthode *addChild*
     // La méthode *addChild* ajoute à l'objet courant un objet
     // enfant.
     addChild(objectName, child) {
-	  child.name = objectName;
-      this.children.push(child);
-	  return child;
+      this.children[objectName] = child;
     }
 
     // ## Fonction *getChild*
     // La fonction *getChild* retourne un objet existant portant le
     // nom spécifié, dont l'objet courant est le parent.
     getChild(objectName) {      
-	  var child = null;
-      this.children.forEach((key) => {
-        if (child) {
-          return;
-        }
-		if(key.name == objectName)
-			child = key;
-      });
-      return child;
+	  return this.children[objectName];
     }
 
     // ## Fonction *fingObjectInScene*
@@ -98,203 +80,43 @@ define([
 	  return current.findObject(objectName);
     }
 	
-	// ## Fonction *fingObjectInObject*
-    // Cette fonction retourne un objet fils d'un objet portant le
-    // nom spécifié.
-	findObjectInObject(objectName){
-	  if(this.name == objectName)
-			return this;
-      if (this.getChild(objectName)) {
-        return this.getChild(objectName);
-      }
-      var object = null;
-      this.children.forEach((key) => {
-        if (object) {
-          return;
-        }
-		if(key)
-			object = key.findObjectInObject(objectName);
-      });
-      return object;
-    }
 
     // ## Méthode *display*
     // Cette méthode appelle la méthode *display* des composants
     // de l'objet.
     display(dT) {
-      this.children.forEach((key) => {
-        key.display(dT);
-      });
-	  this.components.forEach((key) => {
-        key.display(dT);
-      });
-	  return this;
+     for (var keys in this.components){
+        this.components[keys].display(dT);
+      }
+	 for (var keys in this.children){
+        this.children[keys].display(dT);
+      }
     }
 
     // ## Méthode *update*
     // Cette méthode appelle la méthode *update* des composants
     // de l'objet.
     update(dT) {
-     this.children.forEach((key) => {
-        key.update(dT);
-      });
-	  this.components.forEach((key) => {
-        key.update(dT);
-      });
-	 return this;
+     for (var keys in this.components){
+		//console.log(this.components[keys]);
+        this.components[keys].update(dT);
+      }
+	 for (var keys in this.children){
+        this.children[keys].update(dT);
+      }
+	   
     }
+	
+	setup(descr, promises) {
+		for(var i in this.children){
+			this.children[i].setup(descr.children[this.children[i].name], promises);
+		};	
+		for(var i in this.components){
+			this.components[i].setup(descr.components[this.components[i].type], promises);
+		};	
+	}
+	
   }
-
-  // ## Classe *PlayerObject*
-  // Cette classe représente un joueur selon une position,
-  // une texture, un joystick et un collider
-  class PlayerObject extends SceneObject{
-	// ### Constructeur de la classe *PlayerObject*
-    // Le constructeur de cette classe prend en paramètre le name du PlayerObject et l'objet
-    // propriétaire du composant, et l'assigne au membre `owner`.
-	  constructor(name, owner){		  
-		  super(name, owner);
-		  
-		  this.addComponent("Position");
-		  this.addComponent("Texture");		  
-		  this.addComponent("Joystick");
-		  this.addComponent("Collider");
-		  
-		  var score = new ScoreObject("score", this);
-		  this.addChild("score", score);
-	  }
-	  
-	// ### Méthode *setup*
-    // Initilise la position, la texture, le joystick, et le score du player.
-	  setup(descr){
-		  var pos = this.getComponent("Position");
-		  pos.setup(descr["components"]["Position"]);
-		  
-		  var texture = this.getComponent("Texture");
-		  texture.setup(descr["components"]["Texture"]);
-		  
-		  var joystick = this.getComponent("Joystick");
-		  joystick.setup(descr["components"]["Joystick"]);
-		  
-		  var collider = this.getComponent("Collider");
-		  collider.setup(descr["components"]["Collider"]); 
-		  
-		  var score = this.getChild("score");
-		  score.setup(descr["children"]["score"]);
-	  }
-  }
-  
-  // ## Classe *BackgroundObject*
-  // Cette classe représente un background selon une position
-  // et une texture
-  class BackgroundObject extends SceneObject{
-	// ### Constructeur de la classe *PlayerObject*
-    // Le constructeur de cette classe prend en paramètre le name du PlayerObject, l'object
-	// de description et l'objet propriétaire du composant, et l'assigne au membre `owner`.
-	  constructor(name,descr, owner){
-		  super(name,owner);
-		  
-		  this.addComponent("Position");
-		  this.addComponent("Texture");			  
-	  }
-	  
-	// ### Méthode *setup*
-    // Initilise la position et la texture.
-	  setup(descr){
-		  var pos = this.getComponent("Position");
-		  pos.setup(descr["components"]["Position"]);
-		  
-		  var texture = this.getComponent("Texture");
-		  texture.setup(descr["components"]["Texture"]);
-	  }
-  }
-  
-  // ## Classe *RefereeObject*
-  // Cette classe représente un arbitre selon un arbitre
-  class RefereeObject extends SceneObject{
-	 // ### Constructeur de la classe *RefereeObject*
-    // Le constructeur de cette classe prend en paramètre le name du RefereeObject et l'objet
-    // propriétaire du composant, et l'assigne au membre `owner`.
-	  constructor(name, owner){
-		super(name, owner);
-		
-		this.addComponent("Referee");
-	  }
-	  
-	 // ### Méthode *setup*
-    // Initilise l'arbitre.
-	  setup(descr){		
-		var referee = this.getComponent("Referee");
-		referee.setup(descr["components"]["Referee"]);
-	  }
-  }
-  
-  // ## Classe *BallObject*
-  // Cette classe représente une ball selon une position,
-  // une texture, une motion et un collider
-  class BallObject extends SceneObject{
-	// ### Constructeur de la classe *BallObject*
-    // Le constructeur de cette classe prend en paramètre le name du BallObject et l'objet
-    // propriétaire du composant, et l'assigne au membre `owner`.
-	  constructor(name, owner){
-		  super(name,owner);
-		  
-		  this.addComponent("Position");		  
-		  this.addComponent("Texture");
-		  this.addComponent("Motion");
-		  this.addComponent("Collider");	
-	  }
-	  
-	// ### Méthode *setup*
-    // Initilise la position, la texture, la motion, et le collider de la ball.
-	  setup(descr){
-		  var pos = this.getComponent("Position");
-		  pos.setup(descr["components"]["Position"]);
-		  
-		  var texture = this.getComponent("Texture");
-		  texture.setup(descr["components"]["Texture"]);
-		  
-		  var motion = this.getComponent("Motion");
-		  motion.setup(descr["components"]["Motion"]);	
-		  
-		  var collider = this.getComponent("Collider");
-		  collider.setup(descr["components"]["Collider"]);	  
-	  }
-  }
-  
-  // ## Classe *ScoreObject*
-  // Cette classe représente un score selon une position,
-  // une texture, une textureAtlas et un score
-  class ScoreObject extends SceneObject{
-	// ### Constructeur de la classe *ScoreObject*
-    // Le constructeur de cette classe prend en paramètre le name du ScoreObject et l'objet
-    // propriétaire du composant, et l'assigne au membre `owner`.
-	  constructor(name, owner){
-		  super(name,owner);
-		  
-		  this.addComponent("Position");
-		  this.addComponent("Texture");	  
-		  this.addComponent("TextureAtlas");		  
-		  this.addComponent("Score");
-	  }
-	  
-	// ### Méthode *setup*
-    // Initilise la position, la texture, la textureAtlas, et le score du score.
-	  setup(descr){		  
-		  var pos = this.getComponent("Position");
-		  pos.setup(descr["components"]["Position"]);
-		  
-		  var texture = this.getComponent("Texture");
-		  texture.setup(descr["components"]["Texture"]);
-		  
-		  var texture = this.getComponent("TextureAtlas");
-		  texture.setup(descr["components"]["TextureAtlas"]);
-		  
-		  var texture = this.getComponent("Score");
-		  texture.setup(descr["components"]["Score"]);
-	  }
-  }
-
 
   return SceneObject;
 });
